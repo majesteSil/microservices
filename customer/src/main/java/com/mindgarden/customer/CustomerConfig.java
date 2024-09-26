f1920438b3677fc0c74803ca349e6c88e42d1706
franck_majeste <fsilatsadogmo@smail.hs-bremerhaven.de>
2024-09-26 19:43:03 +0200

    kafka and rabbitqm

--- customer/src/main/java/com/mindgarden/customer/CustomerConfig.java
+++ customer/src/main/java/com/mindgarden/customer/CustomerConfig.java
@@ -1,10 +1,18 @@
 package com.mindgarden.customer;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
 public class CustomerConfig
 {
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
