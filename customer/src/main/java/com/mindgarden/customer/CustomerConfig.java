fa755cbc930ba95b8a0603a16890b0bfe1d2369a
franck_majeste <fsilatsadogmo@smail.hs-bremerhaven.de>
2024-09-27 00:04:28 +0200

    wip on kafka

--- customer/src/main/java/com/mindgarden/customer/CustomerConfig.java
+++ customer/src/main/java/com/mindgarden/customer/CustomerConfig.java
@@ -1,8 +1,10 @@
 package com.mindgarden.customer;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -12,7 +14,7 @@ public class CustomerConfig
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public NewTopic customerTopic(){
+        return TopicBuilder.name("mindgarden.customer.topic").build();
     }
-}
+ }
