031d2faae3e1fac2d4db583b33a0115465bb61f1
Majetse Silatsa <silatsafranck@gmail.com>
2024-09-26 16:37:02 +0200

    configure eureka discovery server

--- customer/src/main/java/com/mindgarden/customer/CustomerApplication.java
+++ customer/src/main/java/com/mindgarden/customer/CustomerApplication.java
@@ -3,7 +3,6 @@ package com.mindgarden.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class CustomerApplication
 {
